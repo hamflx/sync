714465852b99c321ff188f543bbab9d77c2ab109
hamflx <a@hamflx.cn>
2024-10-18 09:21:58 +0000

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
+use auth_git2::GitAuthenticator;
 use git2::Repository;
 
 fn main() {
     let repo = Repository::open(std::env::current_dir().unwrap()).unwrap();
-    let remote = repo.find_remote("origin").unwrap();
-    let head = repo.head().unwrap();
-
-    let ref_head = head.resolve().unwrap();
-    println!("ref: {:?}", ref_head.name().unwrap());
-
-    let shorthand = head.shorthand().unwrap();
-    println!("{shorthand}");
-
-    println!("head: {:?}", head.is_branch());
-    let target = head.symbolic_target().unwrap();
-    println!("{target}");
-
-    // remote.push(refspecs, opts)
+    let mut remote = repo.find_remote("origin").unwrap();
+    let head = repo.head().unwrap().resolve().unwrap();
+    let name = head.name().unwrap();
+    if !name.starts_with("refs") {
+        panic!("Invalid Name: {name}");
+    }
+    let auth = GitAuthenticator::default();
+    auth.push(&repo, &mut remote, &[&format!("{name}:{name}")])
+        .unwrap();
 }
